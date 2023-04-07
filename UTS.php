5d49f0dbc1f731f49db83bd3c13966581fe72ebc
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-04-07 21:32:49 +0700

    perkalian aritmatika

--- UTS.php
+++ UTS.php
@@ -1,16 +1,9 @@
 <?php
-  echo"<table align='center'><tr><td align='center'>";
-    for($i=0;$i<=6;$i++){
-       for($a=$i;$a>=0;$a--){
-         print('*');
-       }
-    echo"<br/>";
-   }
-  for($i=0;$i<=5;$i++){
-    for($a=$i;$a<=5;$a++){
-      print('*');
-    }
-  echo"<br/>";
+for ($i = 1; $i <= 15; $i++) {
+  for ($j = 15; $j <= 15; $j++) {
+    $hasil = $i * $i;
+    echo "$i x $i = $hasil<br>";
   }
- echo"</td></tr></table>";
+  echo "<br>";
+}
 ?>
