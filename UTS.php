c1c43ac4d0b53b291320d966894da0dcb5bd54a5
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-04-07 21:15:17 +0700

    belah ketupat bintang

--- UTS.php
+++ UTS.php
@@ -1,10 +1,16 @@
 <?php
-// soal no 5
-$date = new DateTime('2021-05-04');
-
-
-for ($i = 1; $i <= 4; $i++) {
-    echo 'Minggu ke-' . $i . ': ' . $date->format('Y-m-d') . '<br>';
-    $date->modify('next tuesday'); 
-}
+  echo"<table align='center'><tr><td align='center'>";
+    for($i=0;$i<=6;$i++){
+       for($a=$i;$a>=0;$a--){
+         print('*');
+       }
+    echo"<br/>";
+   }
+  for($i=0;$i<=5;$i++){
+    for($a=$i;$a<=5;$a++){
+      print('*');
+    }
+  echo"<br/>";
+  }
+ echo"</td></tr></table>";
 ?>
