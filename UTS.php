cc424ddfc2a913521357cabb141a17598f0d726a
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-04-07 20:26:02 +0700

    no 2

--- UTS.php
+++ UTS.php
@@ -1,5 +1,10 @@
 <?php 
-//soal no 1
-echo date ("l, d-M-Y " , mktime(0,0,0, 12 , 24-60, 2021));
-echo"<br>";
+//soal no 2
+$star=5;
+	for($a=$star;$a>0;$a--){
+	for($b=$star;$b>=$a;$b--){
+		echo "*";
+	}
+	echo "<br>";
+	}
 ?>
