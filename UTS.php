67a2e5ab03909c05b01d1bd21a813fc694d9946b
sayaarisetiawan <ari_22510010@stimata.ac.id>
2023-04-07 20:45:47 +0700

    no 5

--- UTS.php
+++ UTS.php
@@ -1,19 +1,10 @@
-<table border="1" cellpadding="10" cellspacing="0">
-    <?php
-    for ($i = 1; $i <= 10; $i++) {
-        $row_color = ($i % 2 == 0) ? 'white' : 'green';
-        ?>
-        <tr style="background-color: <?= $row_color ?>">
-            <?php
-            for ($j = 1; $j <= 5; $j++) {
-                $cell_value = ($i - 1) * 5 + $j;
-                ?>
-                <td><?= $cell_value ?></td>
-                <?php
-            }
-            ?>
-        </tr>
-        <?php
-    }
-    ?>
-</table>
+<?php
+// soal no 5
+$date = new DateTime('2021-05-04');
+
+
+for ($i = 1; $i <= 4; $i++) {
+    echo 'Minggu ke-' . $i . ': ' . $date->format('Y-m-d') . '<br>';
+    $date->modify('next tuesday'); 
+}
+?>
